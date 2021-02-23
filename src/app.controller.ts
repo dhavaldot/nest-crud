fc914ab9bf93298f5fed9c6eee16da8da14ed347
Dhaval <dhaval.dotinfotech@gmail.com>
2021-02-23 17:50:13 +0530

    line removed

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,7 +1,6 @@
 /* eslint-disable @typescript-eslint/no-empty-function */
 import { Controller, Get, Redirect } from '@nestjs/common';
 import { AppService } from './app.service';
-import { UserService } from './user/user.service';
 
 @Controller()
 export class AppController {
--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,6 +1,5 @@
 /* eslint-disable prefer-const */
-import { UserModel } from './../models/user.model';
-import { flatten, Inject, Injectable } from '@nestjs/common';
+import { Inject, Injectable } from '@nestjs/common';
 import { Model } from 'mongoose';
 import { InjectModel } from '@nestjs/mongoose';
 import { User } from '../interfaces/user.interface';
